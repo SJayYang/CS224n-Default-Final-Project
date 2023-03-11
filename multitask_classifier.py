Fix similarity label normalization formula

multitask_classifier.py
@@ -31,6 +31,7 @@ def seed_everything(seed=11711):
 
 BERT_HIDDEN_SIZE = 768
 N_SENTIMENT_CLASSES = 5
+N_SIMILARITY_CLASSES = 6
 
 
 class MultitaskBERT(nn.Module):
@@ -200,7 +201,7 @@ def train_multitask(args):
 
             train_loss += loss.item()
             num_batches += 1
-        
+
         # STS
         for batch in tqdm(sts_train_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE):
             b_ids_1, b_ids_2, b_mask_1, b_mask_2, b_labels = (batch['token_ids_1'], batch['token_ids_2'], 
@@ -214,7 +215,39 @@ def train_multitask(args):
             b_labels = b_labels.to(device)
 
             # normalize labels            
-            b_labels = b_labels / config.num_labels
+            b_labels = b_labels / (N_SIMILARITY_CLASSES - 1)
+
+            optimizer.zero_grad()
+
+            # compute logits (cosine similarity scores)
+            logits = model.predict_similarity(b_ids_1, b_mask_1, b_ids_2, b_mask_2)
+
+            # run MSE loss between normalized labels and logits
+            loss = F.mse_loss(logits, b_labels)
+            # loss.requires_grad = True
+
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            num_batches += 1
+            
+        # STS
+        for batch in tqdm(sts_train_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE):
+            b_ids_1, b_ids_2, b_mask_1, b_mask_2, b_labels = (batch['token_ids_1'], batch['token_ids_2'], 
+                                                              batch['attention_mask_1'], batch['attention_mask_2'],
+                                                              batch['labels'])
+
+            b_ids_1 = b_ids_1.to(device)
+            b_ids_2 = b_ids_2.to(device)
+            b_mask_1 = b_mask_1.to(device)
+            b_mask_2 = b_mask_2.to(device)
+            b_labels = b_labels.to(device)
+
+            import pdb
+            pdb.set_trace()
+            # normalize labels            
+            b_labels = b_labels / config['num_labels']
 
             optimizer.zero_grad()
 