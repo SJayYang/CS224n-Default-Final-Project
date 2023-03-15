68d650594b9c04361e9c94e592c4b817abb225ed
SJayYang <stanley.j.yang@gmail.com>
2023-03-15 15:46:47 -0700

    Add tasks for finding the relevant indices to confirm

--- evaluation.py
+++ evaluation.py
@@ -67,13 +67,27 @@ def model_eval_pretrain(dataloader, model, device):
         b_ids, b_mask, b_labels, b_sents, b_sent_ids = batch['token_ids'],batch['attention_mask'],  \
                                                         batch['labels'], batch['sents'], batch['sent_ids']
 
+
+        # Get the indices for which words we need to check
+        BERT_mask = batch['BERT_mask']
+        num_true = BERT_mask.sum(dim=1)
+        indices = torch.nonzero(num_true == 1, as_tuple=True)[1]
+        indices[num_true != 1] = -1
+        indices = indices.tolist()
+        # Confirm use TODO
+
         b_ids = b_ids.to(device)
         b_mask = b_mask.to(device)
 
         logits = model.predict_masked_tokens(b_ids, b_mask)
         logits = logits.detach().cpu().numpy()
         preds = np.argmax(logits, axis=1).flatten()
+        # Which of the 55 words do you actually care about
+        import pdb
+        pdb.set_trace()
 
+        # Check to see which word do we care about from b_labels
+        # TODO
         b_labels = b_labels.flatten()
         y_true.extend(b_labels)
         y_pred.extend(preds)
